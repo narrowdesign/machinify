0e9e0d25d2e97483141c3b2aa13c799e8c6bda61
narrowdesign <nick@narrowdesign.com>
2017-07-15 14:20:46 -0400

    touch

--- ui/assets/scripts/script.js
+++ ui/assets/scripts/script.js
@@ -1,6 +1,6 @@
 var _winW = window.innerWidth; // width of the window
 var _winH = window.innerHeight;
-var onLine, scrollX, scrollY, oldScrollX=0, oldScrollY=0, scrollXTotal=0, scrollYTotal=0, scrollXVelocity=0, scrollYVelocity=0, scrollXPercent, scrollYPercent, touchEnabled, touchMoveX, touchMoveY, touchMoveTotal, mouseX, mouseY, oldMouseX=0, oldMouseY=0, mouseXVelocity, mouseYVelocity, mouseXTotal=0, mouseYTotal=0, mouseXPercent, mouseYPercent, mouseXSpeed, mouseYSpeed, cookies, language, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, keyTotal=0, keyCache='', keyCurrent, mouseEnterTotal, mouseLeaveTotal, mouseDragTime, mouseUpEvents=0, mouseDownEvents=0, mouseDownTime, clicks=0, currentDate, startDate, currentTime, startTime, totalTime, activeTime, inactiveTime, plugins, device, OS, OSVersion, browser, browserVersion;
+var onLine, scrollX, scrollY, gyroscopeX, gyroscopeY, oldScrollX=0, oldScrollY=0, scrollXTotal=0, scrollYTotal=0, scrollXVelocity=0, scrollYVelocity=0, scrollXPercent, scrollYPercent, touchEnabled, oldTouchX, oldTouchY, touchX, touchY, touchMoveTotal=0, mouseX, mouseY, oldMouseX=0, oldMouseY=0, mouseXVelocity, mouseYVelocity, mouseXTotal=0, mouseYTotal=0, mouseXPercent, mouseYPercent, mouseXSpeed, mouseYSpeed, cookies, language, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, scrollX, keyTotal=0, keyCache='', keyCurrent, mouseEnterTotal, mouseLeaveTotal, mouseDragTime, mouseUpEvents=0, mouseDownEvents=0, mouseDownTime, clicks=0, currentDate, startDate, currentTime, startTime, totalTime, activeTime, inactiveTime, plugins, device, OS, OSVersion, browser, browserVersion;
 
 var isTouchDevice;
 var mobileSafari;
@@ -52,119 +52,128 @@ function runScript() {
 
     // UTIL EVENTS
     WIN.on('resize',resizeHandler);
-
-    WIN.on('touchstart',function(){
-      isTouchDevice = true;
-    })
     WIN.on('wheel',scrollHandler);
     WIN.on('scroll',scrollHandler);
-    WIN.on('touchmove',scrollHandler);
     WIN.on('mousemove',mousemoveHandler);
     WIN.on('keydown', keydownHandler);
     WIN.on('keyup', keyupHandler);
     WIN.on('mousedown', function(){mouseDownEvents++});
     WIN.on('mouseup', function(){mouseUpEvents++});
+    WIN.on('touchstart', touchStartHandler);
+    WIN.on('touchmove', touchMoveHandler);
+    // WIN.on('touchend', touchEndHandler);
     WIN.on('click', function(){clicks++});
-
-  // FUNCTIONS
-  ////////////
-  (function init() {
-
-    if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) {
-        browser = 'Opera';
-    }
-    else if(navigator.userAgent.indexOf("Chrome") != -1 )
-    {
-        browser = 'Chrome';
-    }
-    else if(navigator.userAgent.indexOf("Safari") != -1)
-    {
-        browser = 'Safari';
-    }
-    else if(navigator.userAgent.indexOf("Firefox") != -1 )
-    {
-         browser = 'Firefox';
-    }
-    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
-    {
-      browser = 'IE';
-    }
-    else
-    {
-       browser = 'unknown';
+    if (window.DeviceOrientationEvent) {
+      window.addEventListener('deviceorientation',function(e){
+        gyroscopeX = e.beta;
+        gyroscopeY = e.gamma;
+      })
+    }else{
+      window.addEventListener("devicemotion",function(e){
+        gyroscopeX = event.acceleration.x * 2;
+        gyroscopeY = event.acceleration.y * 2
+      });
     }
 
-    OS = navigator.platform
-    plugins = navigator.plugins.length;
-    cookies = navigator.cookiesEnables?"enabled":"disabled";
-    language = navigator.language;
-    device = navigator.platform
-    OSVersion = navigator.platform
-    browser = browser
-    browserVersion = navigator.platform
-    startDate = new Date();
-    startTime = startDate.toTimeString();
+    // FUNCTIONS
+    ////////////
+    (function init() {
 
-    $('.output-cookies').text(cookies);
-    $('.output-language').text(language);
-    $('.output-device').text(device);
-    $('.output-OS').text(OS);
-    $('.output-OSVersion').text(OSVersion);
-    $('.output-browser').text(browser);
-    $('.output-browserVersion').text(browserVersion);
+      if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) {
+          browser = 'Opera';
+      }
+      else if(navigator.userAgent.indexOf("Chrome") != -1 )
+      {
+          browser = 'Chrome';
+      }
+      else if(navigator.userAgent.indexOf("Safari") != -1)
+      {
+          browser = 'Safari';
+      }
+      else if(navigator.userAgent.indexOf("Firefox") != -1 )
+      {
+           browser = 'Firefox';
+      }
+      else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+      {
+        browser = 'IE';
+      }
+      else
+      {
+         browser = 'unknown';
+      }
 
-    render()
-  })()
-  function render() {
-    onLine = navigator.onLine?"online":"offline";
-    currentDate = new Date();
-    currentTime = (new Date()).toTimeString()
-    totalTime = currentDate-startDate + 'ms';
+      OS = navigator.platform
+      plugins = navigator.plugins.length;
+      cookies = navigator.cookiesEnables?"enabled":"disabled";
+      language = navigator.language;
+      device = navigator.platform
+      OSVersion = navigator.platform
+      browser = browser
+      browserVersion = navigator.platform
+      startDate = new Date();
+      startTime = startDate.toTimeString();
 
-    $('.output-mouseX').text(mouseX)
-    $('.output-mouseY').text(mouseY)
-    $('.output-mouseXPercent').text(mouseXPercent)
-    $('.output-mouseYPercent').text(mouseYPercent)
-    $('.output-scrollY').text(scrollY)
-    $('.output-scrollX').text(scrollX);
-    $('.output-scrollXTotal').text(scrollXTotal);
-    $('.output-scrollYTotal').text(scrollYTotal);
-    $('.output-scrollXVelocity').text(scrollXVelocity);
-    $('.output-scrollYVelocity').text(scrollYVelocity);
-    $('.output-scrollXPercent').text(scrollXPercent);
-    $('.output-scrollYPercent').text(scrollYPercent);
-    $('.output-touchEnabled').text(touchEnabled);
-    $('.output-touchMoveX').text(touchMoveX);
-    $('.output-touchMoveY').text(touchMoveY);
-    $('.output-touchMoveTotal').text(touchMoveTotal);
-    $('.output-mouseX').text(mouseX);
-    $('.output-mouseY').text(mouseY);
-    $('.output-mouseXVelocity').text(mouseXVelocity);
-    $('.output-mouseYVelocity').text(mouseYVelocity);
-    $('.output-mouseXTotal').text(mouseXTotal);
-    $('.output-mouseYTotal').text(mouseYTotal);
-    $('.output-mouseXVelocity').text(mouseXVelocity);
-    $('.output-mouseYVelocity').text(mouseYVelocity);
-    $('.output-keyTotal').text(keyTotal);
-    $('.output-keyCache').text(keyCache);
-    $('.output-keyCurrent').text(keyCurrent);
-    $('.output-mouseEnterTotal').text(mouseEnterTotal);
-    $('.output-mouseLeaveTotal').text(mouseLeaveTotal);
-    $('.output-mouseDragTime').text(mouseDragTime);
-    $('.output-mouseUpEvents').text(mouseUpEvents);
-    $('.output-mouseDownEvents').text(mouseDownEvents);
-    $('.output-mouseDownTime').text(mouseDownTime);
-    $('.output-clicks').text(clicks);
-    $('.output-startTime').text(startTime);
-    $('.output-currentDate').text(currentDate.toDateString());
-    $('.output-currentTime').text(currentTime);
-    $('.output-totalTime').text(totalTime);
-    $('.output-activeTime').text(activeTime);
-    $('.output-inactiveTime').text(inactiveTime);
-    $('.output-onLine').text(onLine)
+      $('.output-cookies').text(cookies);
+      $('.output-language').text(language);
+      $('.output-device').text(device);
+      $('.output-OS').text(OS);
+      $('.output-OSVersion').text(OSVersion);
+      $('.output-browser').text(browser);
+      $('.output-browserVersion').text(browserVersion);
 
-    requestAnimationFrame(render)
-  }
+      render()
+    })()
+    function render() {
+      onLine = navigator.onLine?"online":"offline";
+      currentDate = new Date();
+      currentTime = (new Date()).toTimeString()
+      totalTime = currentDate-startDate + 'ms';
+
+      $('.output-mouseX').text(mouseX)
+      $('.output-mouseY').text(mouseY)
+      $('.output-mouseXPercent').text(mouseXPercent)
+      $('.output-mouseYPercent').text(mouseYPercent)
+      $('.output-scrollY').text(scrollY)
+      $('.output-scrollX').text(scrollX);
+      $('.output-scrollXTotal').text(scrollXTotal);
+      $('.output-scrollYTotal').text(scrollYTotal);
+      $('.output-scrollXVelocity').text(scrollXVelocity);
+      $('.output-scrollYVelocity').text(scrollYVelocity);
+      $('.output-scrollXPercent').text(scrollXPercent);
+      $('.output-scrollYPercent').text(scrollYPercent);
+      $('.output-touchEnabled').text(touchEnabled);
+      $('.output-touchX').text(touchX);
+      $('.output-touchY').text(touchY);
+      $('.output-touchMoveTotal').text(touchMoveTotal);
+      $('.output-mouseX').text(mouseX);
+      $('.output-mouseY').text(mouseY);
+      $('.output-mouseXVelocity').text(mouseXVelocity);
+      $('.output-mouseYVelocity').text(mouseYVelocity);
+      $('.output-mouseXTotal').text(mouseXTotal);
+      $('.output-mouseYTotal').text(mouseYTotal);
+      $('.output-mouseXVelocity').text(mouseXVelocity);
+      $('.output-mouseYVelocity').text(mouseYVelocity);
+      $('.output-keyTotal').text(keyTotal);
+      $('.output-keyCache').text(keyCache);
+      $('.output-keyCurrent').text(keyCurrent);
+      $('.output-mouseEnterTotal').text(mouseEnterTotal);
+      $('.output-mouseLeaveTotal').text(mouseLeaveTotal);
+      $('.output-mouseDragTime').text(mouseDragTime);
+      $('.output-mouseUpEvents').text(mouseUpEvents);
+      $('.output-mouseDownEvents').text(mouseDownEvents);
+      $('.output-mouseDownTime').text(mouseDownTime);
+      $('.output-clicks').text(clicks);
+      $('.output-startTime').text(startTime);
+      $('.output-currentDate').text(currentDate.toDateString());
+      $('.output-currentTime').text(currentTime);
+      $('.output-totalTime').text(totalTime);
+      $('.output-activeTime').text(activeTime);
+      $('.output-inactiveTime').text(inactiveTime);
+      $('.output-onLine').text(onLine)
+
+      requestAnimationFrame(render)
+    }
     function scrollHandler(e) {
       scrollX = WIN.scrollLeft();
       scrollY = WIN.scrollTop();
@@ -198,6 +207,27 @@ function runScript() {
       $('.header').css({
         transform: 'translate3d(0,' + scrollY/4 + 'px,0)'
       })
+    }
+
+    function touchStartHandler(e) {
+      touchEnabled = "true";
+      touchX = e.originalEvent.touches[0].clientX;
+      touchY = e.originalEvent.touches[0].clientY;
+    }
+    function touchMoveHandler(e) {
+      touchX = e.originalEvent.touches[0].clientX;
+      touchY = e.originalEvent.touches[0].clientY;
+      if (!oldTouchX) {
+        oldTouchX = touchX;
+        oldTouchY = touchY;
+      }
+      touchMoveTotal += Math.abs(touchX - oldTouchX) + Math.abs(touchY - oldTouchY);
+
+      oldTouchX = touchX;
+      oldTouchY = touchY;
+    }
+    function deviceMotionHandler(e) {
+        gyroscopeX = event.acceleration.x * 2
     }
 
     function keydownHandler(e) {
