aabf7c18f4edff0e5a87c781f5d5c2b4a695fb31
narrowdesign <nick@narrowdesign.com>
2017-09-30 21:01:35 -0400

    nav bg

---
+++ ui/assets/scripts/graphicCloud.js
@@ -0,0 +1,138 @@
+(function() {
+  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  window.requestAnimationFrame = requestAnimationFrame;
+
+  var raf;
+
+  var canvas;
+  var ctx;
+  var canvasWidth = 1000;
+  var canvasHeight = 666;
+  var centerX = canvasWidth / 2;
+  var centerY = canvasHeight / 2;
+
+  var radius = canvasHeight / 2.05;
+  var branches = 0;
+  var branchesPos = new Array();
+  var frame = 0;
+  var mR = 0;
+  var mX = 0;
+  var mY = 0;
+
+  var resetting = false;
+
+  var inited = false;
+
+  function init(){
+    inited = true;
+    canvas = document.getElementById("graphic-cloud");
+    canvas.addEventListener('click',function(){
+      resetting = true;
+      resetLines();
+    })
+    ctx = new CanvasWrapper(canvas.getContext("2d"));
+
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    drawLines();
+  }
+
+  function drawLines() {
+    // move the origin to the center
+    ctx.translate(centerX,centerY);
+    // set the number of branches
+    branches = 21;
+    branchesPos = [];
+    for (var i=0;i<branches+500;i++) {
+      branchesPos[i] = [0,0]
+    }
+
+    resetting = false;
+    frame = 0;
+    cancelAnimationFrame(raf);
+    drawTimeout();
+  }
+  var delayTimeout;
+  function drawTimeout(){
+    // draw branches
+    raf = requestAnimationFrame(function(){
+      frame++;
+      if (frame < 200) {
+        for (var i=0;i<branches;i++) {
+          ctx.beginPath();
+          // move the originX point of the line to the end of the previous line
+          var oldX = branchesPos[i][0];
+          var oldY = branchesPos[i][1];
+          ctx.moveTo(oldX,oldY);
+          var shiftX = Math.random()*20 - i%2 - i%3;
+          var shiftY = (Math.random()*20 - i%2 - i%3);
+          rotation = (i/branches)*Math.PI*2;
+          mR += rotation;
+          branchesPos[i] = [oldX + shiftX, oldY + shiftY]
+          ctx.lineTo(branchesPos[i][0],branchesPos[i][1]);
+          ctx.rotate(rotation)
+
+          setStrokeColor(i);
+          ctx.lineWidth = .5;
+          if (branchesPos[i][0] < 233 && branchesPos[i][1] < 233) {
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.rotate(-mR);
+            ctx.translate(-300,0);
+            ctx.moveTo(oldX,oldY);
+            ctx.lineTo(branchesPos[i][0],branchesPos[i][1]);
+            ctx.rotate(mR);
+            ctx.strokeStyle = "black";
+            ctx.stroke();
+            ctx.rotate(-mR);
+            ctx.translate(300,0);
+            ctx.rotate(mR);
+          }
+
+          if (frame > 20) {
+            ctx.beginPath();
+            ctx.arc(Math.min(330,Math.floor(((frame-20)/300)*53)*20), 0, 2, 0, Math.PI*2, false);
+            setStrokeColor(i);
+            ctx.lineWidth = .06;
+            ctx.stroke();
+          }
+        }
+      } else if (!resetting) {
+        resetting = true;
+        setTimeout(function(){
+          resetLines();
+        },2000)
+      }
+
+      if (frame%20 == 0) {
+        branches += 20
+        branches = Math.min(500,branches);
+      }
+
+      drawTimeout()
+    })
+  }
+
+  function setStrokeColor(num) {
+    num%2 ? ctx.strokeStyle="#9DB2ED" : ctx.strokeStyle="#E52C58";
+  }
+
+  function setFillColor(num) {
+    num%3 ? ctx.fillStyle="#9DB2ED" : ctx.fillStyle="#E52C58";
+  }
+
+  function resetLines() {
+    ctx.canvas.resetTransform();
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    drawLines();
+  }
+
+  window.addEventListener('scroll',function(e){
+    if (scrollY > document.getElementById("graphic1").getBoundingClientRect().top - window.innerHeight && !inited) {
+      init();
+    }
+  })
+
+})();
