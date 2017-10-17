b1ef5602b1af68061b3f173253c384f56e68bc10
narrowdesign <nick@narrowdesign.com>
2017-10-16 23:52:01 -0400

    letters gpr

--- ui/assets/scripts/graphicRuntime.js
+++ ui/assets/scripts/graphicRuntime.js
@@ -1,21 +1,30 @@
 // One of my first <canvas> experiments, woop! :D
 
 (function(){
-  var WIN_W = 1000;
-  var WIN_H = 1000;
+  var WIN_W = 666;
+  var WIN_H = 666;
 
   var R = 330;
 
-  var QUANTITY = 20;
+  var QUANTITY = 5;
 
   var canvas;
   var ctx;
   var ctx2;
-  var dots;
 
   var centerX = WIN_W * 0.5;
   var centerY = WIN_H * 0.5;
   var frame = 0;
+  var cycle = 0;
+
+  var sides = 3;
+  var shapes = 15;
+
+  var direction = 'down';
+
+  var visible = false;
+
+  var characters = new Array('<','>','{','}','[',']','/','.',',','”')
 
 
 
@@ -27,76 +36,71 @@
       ctx = canvas.getContext('2d');
       ctx2 = canvas2.getContext('2d');
 
-      createdots();
-
       windowResizeHandler();
 
-      loop();
-    }
-  }
 
-  function createdots() {
-    dots = [];
-
-    for (var i = 0; i < QUANTITY; i++) {
-      var dot;
-
-      dots.push( dot );
+      var inview7 = new Waypoint.Inview({
+        element: document.querySelector('.graphic-runtime-container'),
+        entered: function(direction) {
+          if (!visible) {
+            visible = true;
+            frame = 0;
+            sides = 3;
+            shapes = 15;
+            loop();
+          }
+        },
+        exited: function(direction) {
+          frame = 0;
+          visible = false;
+          setTimeout(function(){
+            ctx.clearRect(0,0,WIN_W,WIN_H);
+            ctx2.clearRect(0,0,WIN_W,WIN_H);
+          },100)
+        }
+      })
     }
   }
 
   function windowResizeHandler() {
-    WIN_W = 1000;
-    WIN_H = 1000;
+    WIN_W = 666;
+    WIN_H = 666;
 
-    canvas.width = 1000;
-    canvas.height = 1000;
-    canvas2.width = 1000;
-    canvas2.height = 1000;
+    canvas.width = 666;
+    canvas.height = 666;
+    canvas2.width = 666;
+    canvas2.height = 666;
   }
 
   function loop() {
+    if (visible) {
+      requestAnimationFrame(function(){
+        frame++;
+        ctx2.clearRect(0,0,WIN_W,WIN_H);
+        ctx2.drawImage(canvas,0,0);
 
-    requestAnimationFrame(function(){
-      frame++;
-      ctx2.clearRect(0,0,WIN_W,WIN_H);
-      ctx2.drawImage(canvas,0,0);
-
-      ctx.clearRect(0,0,WIN_W,WIN_H);
-      ctx.globalAlpha = .85;
-      ctx.drawImage(canvas2,0,0);
-      ctx.globalAlpha = 1;
-      canvas.style.transform = 'rotate(' + frame/1.5 + 'deg)'
-      for (i = 0, len = dots.length; i < len; i++) {
-        ctx.beginPath();
-        ctx.strokeStyle = setStrokeColor(i);
-        ctx.lineWidth = 1;
-        var startAngle = (Math.PI*1.5) + i;
-        var endAngle = Math.PI*1.5 + ((Math.PI*2) + i/2) * (frame/200);
-        if (frame > 300 - i*5) {
-          ctx.strokeStyle = setStrokeColor(i+Math.floor(frame/15));
-        }
-        ctx.arc(centerX, centerY, R - (QUANTITY - i)*15, startAngle, endAngle, false);
-        ctx.stroke();
-        if (frame > 400) {
-          frame = 0;
-        }
-      }
-      loop();
-    })
+        ctx.clearRect(0,0,WIN_W,WIN_H);
+        ctx.globalAlpha = .98;
+        ctx.drawImage(canvas2,0,0);
+        ctx.globalAlpha = 1;
+        setFillColor(frame)
+        ctx.font = Math.random()*400 + 'px sans-serif';
+        var character = characters[Math.floor(Math.random()*characters.length)]
+        ctx.strokeText(character,Math.random()*666,Math.random()*666);
+        loop();
+      })
+    }
   }
 
-  function setStrokeColor(num) {
-    if (num%3 == 0) {
-      ctx.strokeStyle="#9DB2ED"
-    } else if (num%3 == 1) {
-      ctx.strokeStyle="#E52C58"
+  function setFillColor(num) {
+    if (num%2 == 0) {
+      ctx.strokeStyle="#060130"
     } else {
-      ctx.strokeStyle= '#b38ac1'
+      ctx.strokeStyle="#E52C58"
     }
 
   }
 
   init();
 
-}())
+})()
